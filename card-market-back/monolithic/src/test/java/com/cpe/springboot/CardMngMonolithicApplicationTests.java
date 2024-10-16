7ca297d8a29c4e3a3e2b6d50afe84d632e7d205e
louischarnay <louis.charnay@orange.fr>
2024-10-16 15:27:15 +0200

    feat: :sparkles: add card-generator project

--- card-market-back/monolithic/src/test/java/com/cpe/springboot/CardMngMonolithicApplicationTests.java
+++ card-market-back/monolithic/src/test/java/com/cpe/springboot/CardMngMonolithicApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
-public class CardMngMonolithicApplicationTests {
+public class CardGeneratorApplicationTests {
 
 	@Test
 	public void contextLoads() {
