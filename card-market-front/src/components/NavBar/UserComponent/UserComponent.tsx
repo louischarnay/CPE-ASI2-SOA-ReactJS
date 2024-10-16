014010d003c49acc56033552385be843dbbf97fe
Nathan Guillemette <nathan.guillemette73@gmail.com>
2024-10-16 11:54:36 +0200

    login nav to register

--- card-market-front/src/components/NavBar/UserComponent/UserComponent.tsx
+++ card-market-front/src/components/NavBar/UserComponent/UserComponent.tsx
@@ -1,10 +1,15 @@
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import { Link } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 
 const UserComponent = () => {
     const navigate = useNavigate();
 
-    return (<AccountCircleIcon onClick={() => navigate("/login")} />)
+    return (
+        <Link component="button" onClick={() => navigate("/login")}>
+            <AccountCircleIcon />
+        </Link>
+    )
 }
 
 export default UserComponent;
--- card-market-front/src/pages/Login/Login.tsx
+++ card-market-front/src/pages/Login/Login.tsx
@@ -1,7 +1,13 @@
+import { Link } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+
 const Login = () => {
+    const navigate = useNavigate();
+
     return (
         <>
             <h1>Login</h1>
+            <Link component="button" onClick={() => navigate("/register")}>create new account</Link>
         </>
     )
 }
