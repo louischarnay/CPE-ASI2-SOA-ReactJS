3495dcf5d4da407105dd5564f0598707c7f50857
Leon Dumestre <leon.dumestre@gmail.com>
2024-10-16 16:51:55 +0200

    add generators

---
+++ card-market-back/image-generator/src/test/java/com/cpe/imagegenerator/CardgeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cpe.imagegenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImagegeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
