612b11b50ac3e3fded1a8dfa7a9f56b7457e117d
louischarnay <louis.charnay@orange.fr>
2024-10-17 14:21:18 +0200

    feat: :sparkles: add esb in image generator

---
+++ card-market-back/image-generator/src/main/java/com/cpe/imagegenerator/config/ActiveMQRoute.java
@@ -0,0 +1,13 @@
+package com.cpe.imagegenerator.config;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ActiveMQRoute extends RouteBuilder {
+    @Override
+    public void configure() throws Exception {
+        from("direct:sendToGenerateOrchestrator")
+                .to("jms:topic:image-generated");
+    }
+}
