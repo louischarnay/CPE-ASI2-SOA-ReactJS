f3ffa11156fb388c680c265a238c200237c78b5d
Skyward <doriancj.gorse@gmail.com>
2024-10-17 14:51:46 +0200

    Feat: Snackbar for buy and sell

--- card-market-front/src/pages/Buy/Buy.tsx
+++ card-market-front/src/pages/Buy/Buy.tsx
@@ -1,13 +1,23 @@
+import * as React from 'react';
 import CardList from "../../components/Cards/CardList";
+import Snackbar, { SnackbarCloseReason } from '@mui/material/Snackbar';
 
 const Buy = () => {
+    const [open, setOpen] = React.useState(false);
+
     const handleCLick = (e: any) => {
         console.log("carte à acheter")
+        setOpen(true)
     }
     
     return (
         <div>
             <CardList fetchMethod = "all" handleClick={handleCLick}/>
+            <Snackbar
+                open={open}
+                autoHideDuration={6000}
+                message="You just bought a card"
+            />
         </div>
     );
 }
--- card-market-front/src/pages/Sell/Sell.tsx
+++ card-market-front/src/pages/Sell/Sell.tsx
@@ -1,15 +1,23 @@
+import * as React from 'react';
 import CardList from "../../components/Cards/CardList";
+import Snackbar, { SnackbarCloseReason } from '@mui/material/Snackbar';
 
 const Sell = () => {
+    const [open, setOpen] = React.useState(false);
+
     const handleCLick = async (id: any) => {
         const response = null
-
-        // TODO NOTIF
+        setOpen(true)
     }
     
     return (
         <div>
             <CardList fetchMethod="user" handleClick={handleCLick}/>
+            <Snackbar
+                open={open}
+                autoHideDuration={6000}
+                message="You just sold your card"
+            />
         </div>
     );
 }
