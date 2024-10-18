3458cd3f2500f15dca576095fdc66821e32ec950
louischarnay <louis.charnay@orange.fr>
2024-10-18 16:10:59 +0200

    fix: :wrench: delete + and = in active mq data

--- card-market-back/card-generator/src/main/java/com/cpe/cardgenerator/controller/CardGenerator.java
+++ card-market-back/card-generator/src/main/java/com/cpe/cardgenerator/controller/CardGenerator.java
@@ -1,6 +1,9 @@
 package com.cpe.cardgenerator.controller;
 
 import com.cpe.cardgenerator.message.MessageService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Getter;
 import org.apache.camel.ProducerTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,11 +19,24 @@ public class CardGenerator {
         @Autowired
         private MessageService messageService;
 
+        @Autowired
+        private ObjectMapper objectMapper;
+
+        // Request class to hold both prompts
+        @Getter
+        public static class CardGenerationRequest {
+            // Getters and Setters
+            private String imagePrompt;
+            private String descPrompt;
+        }
+
         @PostMapping("/generateCard")
-        public String generateCard(@RequestBody String imagePrompt, @RequestBody String descPrompt) {
+        public String generateCard(@RequestBody CardGenerationRequest request) throws JsonProcessingException {
                 Long id = messageService.createNewMessageEntry();
-                producerTemplate.sendBodyAndHeader("direct:sendToGenerateImage", imagePrompt, "id", id);
-                producerTemplate.sendBodyAndHeader("direct:sendToGenerateDesc", descPrompt, "id", id);
+                String imagePromptJson = objectMapper.writeValueAsString(request.getImagePrompt());
+                String descPromptJson = objectMapper.writeValueAsString(request.getDescPrompt());
+                producerTemplate.sendBodyAndHeader("direct:sendToGenerateImage", imagePromptJson, "id", id);
+                producerTemplate.sendBodyAndHeader("direct:sendToGenerateDesc", descPromptJson, "id", id);
                 return "Card generation initiated with ID: " + id;
         }
 }
