27e2ce991d4360c53a82e3ca3e83b0ceb404066a
Leon Dumestre <leon.dumestre@gmail.com>
2024-11-06 09:01:33 +0100

    feat: add room socket

--- back/game/index.ts
+++ back/game/index.ts
@@ -1,10 +1,8 @@
 import express, { Express, Request, Response } from "express";
-import stompit, { Client } from "stompit";
 import { Server, Socket } from 'socket.io';
 import { createServer } from 'http';
-import { MessageSentByClient } from "./models/message.model";
-import { UserService } from "./services/user.service";
 import { ChatSocket } from "./sockets/chat.socket";
+import { RoomSocket } from "./sockets/room.socket";
 
 const app: Express = express();
 const PORT = 4000;
@@ -21,13 +19,14 @@ app.get("/", (req: Request, res: Response) => {
     }
   });
 
-  const userService = new UserService();
-
   ioServer.on('connection', (socket: Socket) => {
     console.log('a user connected');
 
     const chatSocket = new ChatSocket(socket);
+    const roomSocket = new RoomSocket(socket);
+
     chatSocket.init();
+    roomSocket.init();
   });
 });
 
