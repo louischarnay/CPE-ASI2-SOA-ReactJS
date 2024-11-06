52c981fee95732993486fe860aca7dc108be6572
Leon Dumestre <leon.dumestre@gmail.com>
2024-11-06 08:32:38 +0100

    fix: move chat socket in file

--- back/game/index.ts
+++ back/game/index.ts
@@ -1,74 +1,33 @@
 import express, { Express, Request, Response } from "express";
-import stompit from "stompit";
-import { Server } from 'socket.io';
+import stompit, { Client } from "stompit";
+import { Server, Socket } from 'socket.io';
 import { createServer } from 'http';
 import { MessageSentByClient } from "./models/message.model";
 import { UserService } from "./services/user.service";
+import { ChatSocket } from "./sockets/chat.socket";
 
 const app: Express = express();
 const PORT = 4000;
 
 const server = createServer(app);
 
-const esb = {
-  host: 'localhost',
-  port: 61613,
-  connectHeaders: {
-    host: '/'
-  }
-};
-
 app.get("/", (req: Request, res: Response) => {
   res.send("Backend Node");
 
-const ioServer = new Server(server, {
-  cors: {
-    origin: "http://frontend:3000",
-    methods: ["GET"]
-  }
-});
-
-const userService = new UserService();
-
-ioServer.on('connection', (socket) => {
-  userService.fetchAllUsers();
-  console.log('a user connected');
-
-  socket.on('message-send', async (data: MessageSentByClient) => {
-    console.log('Data received from client:', data);
-
-    const userName = await userService.getUserName(data.userId);
-    if (!userName) {
-      console.error(`User with ID ${data.userId} not found`);
-      return;
+  const ioServer = new Server(server, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"]
     }
+  });
 
-    stompit.connect(esb, (error: Error | null, client: stompit.Client) => {
-      
-      if (error) {
-        console.error('Connection error:', error.message);
-        return;
-      }
-  
-      const sendHeaders = {
-        'destination': '/queue/test',
-        'content-type': 'application/json',
-      };
-  
-      const frame = client.send(sendHeaders);
-      frame.write(JSON.stringify(data));
-      frame.end();
-    });
+  const userService = new UserService();
 
-    const messageReceived = {
-      userId: data.userId,
-      userName,
-      content: data.content,
-      date: new Date(),
-    };
-    
-    socket.emit('message-receive', messageReceived);
-    });
+  ioServer.on('connection', (socket: Socket) => {
+    console.log('a user connected');
+
+    const chatSocket = new ChatSocket(socket);
+    chatSocket.init();
   });
 });
 
---
+++ back/game/sockets/chat.socket.ts
@@ -0,0 +1,63 @@
+import { Client } from "stompit";
+import { UserService } from "../services/user.service";
+import { MessageSentByClient } from "../models/message.model";
+import { Socket } from "socket.io";
+import stompit from "stompit";
+
+const ESB_CONFIG = {
+  host: 'localhost',
+  port: 61613,
+  connectHeaders: {
+    host: '/'
+  }
+};
+
+const MESSAGE_SEND_EVENT = 'message-send';
+const MESSAGE_RECEIVE_EVENT = 'message-receive';
+
+export class ChatSocket {
+  private readonly userService: UserService
+
+  constructor(private readonly socket: Socket) {
+    this.userService = new UserService();
+    this.userService.fetchAllUsers();
+  }
+
+  init() {
+    this.socket.on(MESSAGE_SEND_EVENT, async (data: MessageSentByClient) => {
+      console.log('Data received from client:', data);
+
+      const userName = await this.userService.getUserName(data.userId);
+      if (!userName) {
+        console.error(`User with ID ${data.userId} not found`);
+        return;
+      }
+
+      stompit.connect(ESB_CONFIG, (error: Error | null, client: Client) => {
+
+        if (error) {
+          console.error('Connection error:', error.message);
+          return;
+        }
+
+        const sendHeaders = {
+          'destination': '/queue/test',
+          'content-type': 'application/json',
+        };
+
+        const frame = client.send(sendHeaders);
+        frame.write(JSON.stringify(data));
+        frame.end();
+      });
+
+      const messageReceived = {
+        userId: data.userId,
+        userName,
+        content: data.content,
+        date: new Date(),
+      };
+
+      this.socket.emit(MESSAGE_RECEIVE_EVENT, messageReceived);
+    });
+  }
+}
