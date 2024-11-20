9e4e6c24589e592420cce678d6714915099d6729
Nathan Guillemette <nathan.guillemette73@gmail.com>
2024-11-20 11:49:33 +0100

    chat visibility

--- front/src/routes/Root.tsx
+++ front/src/routes/Root.tsx
@@ -1,12 +1,18 @@
+import { useState } from "react";
 import { Outlet } from "react-router-dom";
 import NavBar from "../components/NavBar/Navbar";
 import ChatContainer from "../components/Chat/ChatContainer";
 import User from "../models/user.model";
 import { useSelector } from "react-redux";
-import "./Root.css"
+import "./Root.css";
 
 export default function Root() {
   const currentUser: User = useSelector((state: any) => state.userReducer.currentUser);
+  const [isChatVisible, setIsChatVisible] = useState(false); // État pour gérer la visibilité du chat
+
+  const toggleChatVisibility = () => {
+    setIsChatVisible((prev) => !prev); // Inverse l'état
+  };
 
   return (
     <>
@@ -14,12 +20,18 @@ export default function Root() {
         <NavBar />
       </div>
       <div id="container">
-        {
-          currentUser &&
+        {currentUser && (
+          <div id="chat-toggle">
+            <button onClick={toggleChatVisibility}>
+              {isChatVisible ? "✖️" : "💬"}
+            </button>
+          </div>
+        )}
+        {currentUser && isChatVisible && (
           <div id="chat">
             <ChatContainer />
           </div>
-        }
+        )}
         <div id="content">
           <Outlet />
         </div>
